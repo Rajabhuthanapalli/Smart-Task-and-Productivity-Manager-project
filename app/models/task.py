dbe1a8b5c2ac42402c6ddd0af49d06518d07544c
Rajabhuthanapalli <Rajasekhar.bhuthanapalli@technivioit.com>
2026-02-06 09:35:25 -0600

    Create task.py

---
+++ app/models/task.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from app.database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    is_completed = Column(Boolean, default=False)
+    is_deleted = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
